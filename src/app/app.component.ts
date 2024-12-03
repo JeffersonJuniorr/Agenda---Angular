564a73ebf90d3f3b4db49b02976ccd4b6fbade84
Jefferson <jefferosnjunior1645@gmail.com>
2024-12-03 13:51:01 -0300

    Projeto Agenda em Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
+import { CommonModule } from '@angular/common';
+import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
+import { SeparadorComponent } from './components/separador/separador.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import agenda from "./agenda.json"
+import { FormsModule } from '@angular/forms';
+
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    ContainerComponent, 
+    CommonModule, 
+    CabecalhoComponent,  
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
+   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AgendaA';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = ''
+
+  filtrarContatosPorTexto () : Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos
+    }
+    return this.contatos.filter(contato => {
+      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase())
+    })
+    
+  }
+
+  filtrarContatosPorLetraInicial(letra:string) : Contato[] {
+    return this.filtrarContatosPorTexto().filter( contato => {
+      return contato.nome.toLowerCase().startsWith(letra)
+    })
+  }
 }
